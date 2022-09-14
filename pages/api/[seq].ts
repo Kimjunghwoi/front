a76edf681fafc226bf529a3f577c797f2960800a
Kimjunghwoi <poiuy5947@gmail.com>
2022-09-14 15:41:37 +0900

    rest...api..

--- pages/api/[seq].ts
+++ pages/api/[seq].ts
@@ -2,18 +2,18 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default function userHandler(req: NextApiRequest, res: NextApiResponse) {
     const {
-        query: { id, name },
+        query: { seq, email, name },
         method,
     } = req
 
     switch (method) {
         case 'GET':
             // Get data from your database
-            res.status(200).json({ id, name: `User ${id}` })
+            res.status(200).json({ seq, name: `User ${seq}` })
             break
         case 'PUT':
             // Update or create data in your database
-            res.status(200).json({ id, name: name || `User ${id}` })
+            res.status(200).json({ seq, name: name || `User ${seq}` })
             break
         default:
             res.setHeader('Allow', ['GET', 'PUT'])
